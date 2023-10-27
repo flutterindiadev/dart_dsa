0e9dee075a444051107c57fbc4dda3efdaed3e36
Neosoft <neosoft@Neosoft-Mac-M1.local>
2023-10-27 15:29:56 +0530

    pattern 5 completed

--- lib/dsa_practice.dart
+++ lib/dsa_practice.dart
@@ -1,10 +1,75 @@
 import 'dart:io';
 
 void main() {
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
-      stdout.write("*");
+  pattern5(5);
+}
+
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+void pattern1(int count) {
+  for (int i = 0; i < count; i++) {
+    for (int j = 0; j < count; j++) {
+      stdout.write("* ");
     }
+    stdout.write("\n");
+  }
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+void pattern2(int count) {
+  for (int i = 0; i < count; i++) {
+    stdout.write("* " * (i + 1));
+    stdout.write("\n");
+  }
+}
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+void pattern3(int count) {
+  for (int i = 1; i < count + 1; i++) {
+    for (var j = 1; j < i + 1; j++) {
+      stdout.write("$j ");
+    }
+    stdout.write("\n");
+  }
+}
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+void pattern4(int count) {
+  for (int i = 1; i < count + 1; i++) {
+    for (var j = 1; j < i + 1; j++) {
+      stdout.write("$i ");
+    }
+    stdout.write("\n");
+  }
+}
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+void pattern5(int count) {
+  for (int i = 0; i < count; i++) {
+    stdout.write("* " * (count - i));
     stdout.write("\n");
   }
 }
